98871a943e74b3127539ae76b2e8bfef97784afd
David Bos <david.bos@outlook.com>
2024-10-24 17:46:43 +0200

    Init paging and print

---
+++ README.md
@@ -0,0 +1,17 @@
+Build process designed for win11
+
+### Requirements
+    Docker
+    Qemu
+
+### Setup
+    Add Qemu to path
+
+ - `docker build buildenv -t myos-buildenv`
+
+### Run: 
+start Run.bat
+
+### Delete
+
+ - `docker rmi myos-buildenv -f`
---
+++ Run.bat
@@ -0,0 +1,32 @@
+@echo off
+title OS Runner - Building
+cls
+echo Building!
+echo.
+echo.
+docker run --rm -it -v "%cd%":/root/env myos-buildenv make build-x86_64
+
+if %errorlevel% neq 0 (
+    echo.
+    echo.
+    echo Build failed!
+    echo.
+    echo Press any key to exit!
+    pause >nul
+    exit /b %errorlevel%
+)
+
+echo.
+echo.
+echo Build Succeeded!
+echo.
+echo Press any key to run!
+pause >nul
+title OS Runner - Running
+cls
+echo Make sure to close qemu before building again!
+echo.
+qemu-system-x86_64 -cdrom dist/x86_64/kernel.iso
+echo.
+echo Press any key to exit!
+pause >nul
